c087d34cff2ee6c837087068e9c0f10a53e3a70d
Awesa <awesaatyanta.p@gmail.com>
2022-05-14 12:58:45 +0700

    Pertemuan 14

--- controllers/index.js
+++ controllers/index.js
@@ -2,40 +2,107 @@ const bcrypt = require("bcrypt");
 const User = require("../models").User;
 
 const homeIndex = (req, res) => {
-  const { nama } = req.session;
-  res.render("index.ejs", { nama });
+    const { nama } = req.session;
+    res.render("index.ejs", { nama, loggedIn: req.session.authenticate });
 };
 
 const loginIndex = (req, res) => {
-  // if (req.query.name) {
-  //   res.render('index.ejs', { nama: req.query.name })
-  // }
-  res.render("login.ejs");
+    res.render("login.ejs", { loggedIn: req.session.authenticate });
+    // if (req.session.authenticate) {
+    //     res.redirect("/");
+    // } else {
+    //     res.render("login.ejs");
+    // }
 };
 
 const loginAction = async (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    res.render("login.ejs", { error: "Email dan password diperlukan" });
-  }
+    const { email, password } = req.body;
+    if (!email || !password) {
+        res.render("login.ejs", {
+            error: "Email dan password diperlukan",
+            loggedIn: req.session.authenticate,
+        });
+    }
 
-  const user = await User.findOne({ where: { email } });
-  if (!user) {
-    res.render("login.ejs", { error: "User not found" });
-  } else {
-    const isPassValid = await bcrypt.compare(password, user.password);
-    if (!isPassValid) res.render("login.ejs", { error: "Invalid credential" });
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+        res.render("login.ejs", {
+            error: "User not found",
+            loggedIn: req.session.authenticate,
+        });
+    } else {
+        const isPassValid = await bcrypt.compare(password, user.password);
+        if (!isPassValid) {
+            res.render("login.ejs", {
+                error: "Invalid credential",
+                loggedIn: req.session.authenticate,
+            });
+            return;
+        }
 
-    req.session.authenticate = true;
-    req.session.nama = user.firstName;
+        req.session.authenticate = true;
+        req.session.nama = user.firstName;
 
+        res.redirect("/");
+    }
+};
+
+const logoutAction = (req, res) => {
+    if (req.session.authenticate) {
+        // cek apakah user sudah terlogin
+        // Kalau user sudah terlogin, logout si user
+        req.session.destroy((err) => {
+            console.log("Error");
+        });
+    }
     res.redirect("/");
-  }
+};
+
+const registerIndex = (req, res) => {
+    if (!req.session.authenticate) {
+        res.render("register.ejs", { loggedIn: req.session.authenticate });
+    }
+    res.redirect("/");
+};
+
+const registerAction = async (req, res) => {
+    const { firstName, lastName, email, password, repeatPassword } = req.body;
+    if (!firstName || !lastName || !email || !password || !repeatPassword) {
+        res.render("register.ejs", {
+            error: "Lengkapi semua data yang diperlukan",
+            loggedIn: req.session.authenticate,
+        });
+    }
+
+    const existingUser = await User.findOne({ where: { email } });
+
+    if (existingUser) {
+        res.render("register.ejs", {
+            error: "Email sudah terdaftar",
+            loggedIn: req.session.authenticate,
+        });
+    } else if (password !== repeatPassword) {
+        res.render("register.ejs", {
+            error: "Password tidak sama!",
+            loggedIn: req.session.authenticate,
+        });
+    } else {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = await User.create({
+            firstName,
+            lastName,
+            email,
+            password: hashedPassword,
+        });
+        if (newUser) {
+            res.redirect("/login");
+        }
+    }
 };
 
 const idIndex = (req, res) => {
-  const { id } = req.params;
-  res.send(`Access Route with parms (${id})`);
+    const { id } = req.params;
+    res.send(`Access Route with params (${id})`);
 };
 
 // Request
@@ -51,8 +118,11 @@ const idIndex = (req, res) => {
 // /books/1
 
 module.exports = {
-  homeIndex,
-  loginIndex,
-  loginAction,
-  idIndex,
+    homeIndex,
+    loginIndex,
+    loginAction,
+    idIndex,
+    logoutAction,
+    registerIndex,
+    registerAction,
 };
--- routes.js
+++ routes.js
@@ -1,10 +1,26 @@
-const express = require('express')
-const { homeIndex, loginIndex, loginAction, idIndex } = require('./controllers')
-const router = express.Router()
+const express = require("express");
+const {
+    homeIndex,
+    loginIndex,
+    loginAction,
+    idIndex,
+    logoutAction,
+    registerIndex,
+    registerAction,
+} = require("./controllers");
+const router = express.Router();
 
-router.get('/', homeIndex) // ctrl + space
-router.get('/login', loginIndex)
-router.post('/login', loginAction)
-router.get('/:id', idIndex)
+router.get("/", homeIndex); // ctrl + space
 
-module.exports = router
+// Authentication
+router.get("/login", loginIndex);
+router.post("/login", loginAction);
+
+router.post("/logout", logoutAction);
+
+router.get("/register", registerIndex);
+router.post("/register", registerAction);
+
+router.get("/:id", idIndex);
+
+module.exports = router;
